e535f4df8e7899ac92a7b2b3a0720d510230a940
MariusLevin <pecchioli.mathieu@gmail.com>
2020-06-28 11:23:06 +0200

    Change grid attributes visibility from public to private

--- data_structure/grid.py
+++ data_structure/grid.py
@@ -4,9 +4,9 @@ from numpy import zeros
 class Grid:
     def __init__(self):
         self.name = "Sudoku grid"
-        self.matrix = zeros(shape=(9, 9))
-        self.row = 9
-        self.col = 9
+        self.__matrix = zeros(shape=(9, 9))
+        self.__number_of_row = 9
+        self.__number_of_col = 9
 
     def set_grid(self, sudoku_array):
         """
@@ -14,8 +14,8 @@ class Grid:
 
         :param sudoku_array: numpy array containing the sudoku grid
         """
-        self.matrix = sudoku_array
-        self.row, self.col = self.matrix.shape
+        self.__matrix = sudoku_array
+        self.__number_of_row, self.__number_of_col = self.__matrix.shape
 
     def compare(self, grid_to_compare):
         """
@@ -24,13 +24,13 @@ class Grid:
         :param grid_to_compare: Grid to compare
         :return: Return the number of valid and wrong cells
         """
-        result = zeros(shape=(self.row, self.col))
+        result = zeros(shape=(self.__number_of_row, self.__number_of_col))
         cpt_true = 0
         cpt_false = 0
 
-        for row in range(0, self.row):
-            for col in range(0, self.col):
-                if self.matrix[row, col] == grid_to_compare.matrix[row, col]:
+        for row in range(0, self.__number_of_row):
+            for col in range(0, self.__number_of_col):
+                if self.__matrix[row, col] == grid_to_compare.__matrix[row, col]:
                     result[row, col] = 0
                     cpt_false += 1
                 else:
